6bde59c7bb5299a8e1e000f2b3ef5712f841f962
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-01-13 17:09:13 -0300

    primeiro commit

---
+++ Loops/loops.js
@@ -0,0 +1,23 @@
+
+// for
+
+for ( i = 1; i <= 10 ; i++){
+    console.log(i)
+}
+
+// While, variavel é declarada fora do loop
+
+let i = 1
+while ( i <= 10){
+    console.log(i)
+    i++
+}
+
+
+// Do While
+
+i = 1
+do {
+    console.log(i)
+    i++
+} while (i <= 10)
