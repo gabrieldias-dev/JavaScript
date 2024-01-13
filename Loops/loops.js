50d8c4940e2908d7cf30eaf1146bd42f0acf307e
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-01-13 17:23:22 -0300

    att loops

--- Loops/loops.js
+++ Loops/loops.js
@@ -1,23 +1,45 @@
 
-// for
+// // for
 
-for ( i = 1; i <= 10 ; i++){
+//  for ( i = 1; i <= 10 ; i++){
+//      console.log(i)
+//  }
+
+// // // While, variavel é declarada fora do loop
+
+//  let i = 1
+//  while ( i <= 10){
+//     console.log(i)
+//     i++
+//  }
+
+
+// // // Do While
+
+//  i = 1
+//  do {
+//      console.log(i)
+//     i++
+//  } while (i <= 10)
+
+
+// //  for in loop
+
+// const myCar = {
+//     model: 'BMW',
+//     ano: 2023,
+//     km: 6800
+// }
+
+// for ( let i in myCar){
+//     console.log(i, myCar[i])
+// }
+
+
+//  for of loop
+
+const friends = ['Daniel', 'Lary', 'José', 'Guilherme']
+
+for (let i of friends) {
     console.log(i)
 }
-
-// While, variavel é declarada fora do loop
-
-let i = 1
-while ( i <= 10){
-    console.log(i)
-    i++
-}
-
-
-// Do While
-
-i = 1
-do {
-    console.log(i)
-    i++
-} while (i <= 10)
